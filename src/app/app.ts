5dec7a93fca82bd6283715731944a467f6e9e5c4
Manjunath Naik <raddymanju317@gmail.com>
2025-12-31 21:12:48 +0530

    Initial commit - Angular portfolio project

---
+++ src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from './shared/navbar/navbar';
+import { Hero } from './sections/hero/hero';
+import { About } from './sections/about/about';
+import { Contact } from './sections/contact/contact';
+import { Experience } from './sections/experience/experience';
+import { Projects } from './sections/projects/projects';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [About,Contact,Navbar,Experience,Hero,Projects],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('PortPolio');
+}
